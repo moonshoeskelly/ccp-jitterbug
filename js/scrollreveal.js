d5f6eb3fad66c9df7878f5f4da50755a0cc49e0e
Kelly DeMent <kdement@ufl.edu>
2020-03-22 16:07:41 -0400

    Add scroll reveal JS to colors page

---
+++ js/scrollreveal.js
@@ -0,0 +1,10 @@
+
+
+ScrollReveal().reveal('.colors-1', { delay: 150 });
+ScrollReveal().reveal('.colors-2', { delay: 150 });
+ScrollReveal().reveal('.colors-3', { delay: 150 });
+ScrollReveal().reveal('.colors-4', { delay: 150 });
+ScrollReveal().reveal('.colors-5', { delay: 150 });
+ScrollReveal().reveal('.colors-6', { delay: 150 });
+ScrollReveal().reveal('.colors-7', { delay: 150 });
+ScrollReveal().reveal('.colors-8', { delay: 150 });
