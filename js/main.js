b1aac17d3f71cfd7e4452605870ec6434ec0850a
Kelly DeMent <kdement@ufl.edu>
2020-04-17 19:13:20 -0400

    Add focus to contact form

--- js/main.js
+++ js/main.js
@@ -1,6 +1,5 @@
 // Change homepage navbar color to opaque on scroll
 var myNav = document.getElementById('mynav');
-console.log(myNav);
 
 window.addEventListener('scroll', function () {
         if (document.body.scrollTop >= 50 || document.documentElement.scrollTop >= 50 ) {
