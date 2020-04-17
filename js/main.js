de158b15fa2c9fe0960d4f2f51f9f1287cca3460
Kelly DeMent <kdement@ufl.edu>
2020-04-17 16:46:07 -0400

    Add opaque nav on scroll to homepage

--- js/main.js
+++ js/main.js
@@ -1,6 +1,18 @@
+// Change homepage navbar color to opaque on scroll
+var myNav = document.getElementById('mynav');
+console.log(myNav);
+
+window.addEventListener('scroll', function () {
+        if (document.body.scrollTop >= 50 || document.documentElement.scrollTop >= 50 ) {
+                myNav.classList.add('nav-colored');
+                myNav.classList.remove('nav-transparent');
+            } else {
+                myNav.classList.add('nav-transparent');
+                myNav.classList.remove('nav-colored');
+            }
+    });
+
 //Home page carousel
 jQuery(document).ready(function(){
-  jQuery('.main-social-carousel').slippry()
+  jQuery('.main-social-carousel').slippry();
 });
-
-// Change homepage navbar color to opaque on scroll
